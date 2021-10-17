a94807f0d8d02f0319604b094b2127e6301d073b
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2021-10-17 18:49:30 +0200

    Try to add macos build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
+if [[ -n "${MACOSX_DEPLOYMENT_TARGET}" ]] ; then
+    export CFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${CFLAGS}"
+    export CXXFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${CXXFLAGS}"
+    export LDFLAGS="-isysroot ${CONDA_BUILD_SYSROOT} ${LDFLAGS}"
+fi
+
+
 scons APR="${PREFIX}" APU="${PREFIX}" \
     OPENSSL="$PREFIX" ZLIB="$PREFIX" \
     PREFIX="$PREFIX" CC="$CC" \
