d43b96e1f7c8c2e7b832c77b984cb52a894cd153
Rich Fiorella <rfiorella@lanl.gov>
2024-10-09 19:29:23 -0400

    try to fix bad CPU error on arm64

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -ex
 if [ $CONDA_BUILD_CROSS_COMPILATION -eq 1 ]; then
   scons APR="${PREFIX}" APU="${PREFIX}" \
       OPENSSL="$PREFIX" ZLIB="$PREFIX" \
-      PREFIX="$PREFIX" CC="$CC_FOR_BUILD" \
+      PREFIX="$PREFIX" CC="$CC" \
       CFLAGS="$CFLAGS" \
       CPPFLAGS="$CPPFLAGS" LINKFLAGS="$LDFLAGS"
 else 
